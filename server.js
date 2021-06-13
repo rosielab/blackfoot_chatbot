d4328aacc9dae4cf849c2e2e17c6fac420731be7
ptuttosi <ptuttosi@gmail.com>
2021-06-13 10:15:06 -0700

    add production build
    The production build is written to build/
    Server.js now prints the port when connected

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+const port = (process.env.PORT || 8080)
+
+app.listen(port, () => {
+  console.log(`AIS Scraper App listening at: http://app:${port}`)
+});
