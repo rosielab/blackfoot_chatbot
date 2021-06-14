ca2273121659a5f4c8e5f5d07e97745ef52ab632
dannyyua <dannyyua@gmail.com>
2021-06-13 20:14:06 -0700

    modified score saving to use local storage instead of cookies, small edits

--- src/scenes/ScoresScene.js
+++ src/scenes/ScoresScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 
-import { scores, scenes } from './util.js';
+import { scenes } from './util.js';
 
 export default class ScoresScene extends Phaser.Scene {
   constructor() {
@@ -26,133 +26,135 @@ export default class ScoresScene extends Phaser.Scene {
     const back = this.add.image(53, 548, 'back');
     const back1 = this.add.image(53, 548, 'back1');
 
-    function scrollDownScores(scenes_index, scores_txt) {
-      if (scenes_index + 5 < scenes.length - 1) {
-        var scoresText = '';
-        for (var i = scenes_index + 5; i < scenes_index + 10; i++) {
-          if (i < scenes.length - 1) {
-            // only append the "all" score to the end
-            scoresText = scoresText.concat(
-              scenes[i][0].toUpperCase() +
-                scenes[i].slice(1) +
-                ': ' +
-                scores[scenes[i]] +
-                '/10\n'
-            );
-          } else {
-            scoresText = scoresText.concat('\n');
-          }
-        }
-        scoresText = scoresText.concat('All: ' + scores.all + '/10');
-        scores_txt.setText(scoresText);
-        return scenes_index + 5;
-      }
-      return scenes_index;
-    }
+    /*
+      These two functions need to be redone once more scenes are added
+    */
+    // function scrollDownScores(scenes_index, scores_txt) {
+    //   if (scenes_index + 5 < scenes.length - 1) {
+    //     var scoresText = '';
+    //     for (var i = scenes_index + 5; i < scenes_index + 10; i++) {
+    //       if (i < scenes.length - 1) {
+    //         // only append the "all" score to the end
+    //         scoresText = scoresText.concat(
+    //           scenes[i][0].toUpperCase() +
+    //             scenes[i].slice(1) +
+    //             ': ' +
+    //             scores[scenes[i]] +
+    //             '/10\n'
+    //         );
+    //       } else {
+    //         scoresText = scoresText.concat('\n');
+    //       }
+    //     }
+    //     scoresText = scoresText.concat('All: ' + scores.all + '/10');
+    //     scores_txt.setText(scoresText);
+    //     return scenes_index + 5;
+    //   }
+    //   return scenes_index;
+    // }
 
-    function scrollUpScores(scenes_index, scores_txt) {
-      if (scenes_index - 5 >= 0) {
-        return scrollDownScores(scenes_index - 10, scores_txt);
-      }
-      return scenes_index;
-    }
+    // function scrollUpScores(scenes_index, scores_txt) {
+    //   if (scenes_index - 5 >= 0) {
+    //     return scrollDownScores(scenes_index - 10, scores_txt);
+    //   }
+    //   return scenes_index;
+    // }
 
-    function checkCookie(cookie) {
-      const split_cookies = document.cookie.split(';');
-      for (var i = 0; i < split_cookies.length; i++) {
-        if (split_cookies[i].includes(cookie + '=')) {
-          return i;
-        }
-      }
-      return -1;
-    }
+    // remove old score cookie
+    document.cookie = "score= ;expires=Thu, 01 Jan 1970 00:00:00 GMT";
 
-    function updateScoreCookie() {
-      var score_cookie = 'score=';
-      for (var i = 0; i < scenes.length; i++) {
-        score_cookie = score_cookie.concat(scores[scenes[i]].toString(16));
-      }
-
-      // set expiry date to 2 years
-      const date = new Date();
-      date.setFullYear(date.getFullYear() + 2);
-      score_cookie = score_cookie.concat('; expires=' + date.toUTCString());
-      document.cookie = score_cookie;
-    }
-
-    // Check if scores have been saved previously
-    const cookie_index = checkCookie('score');
-    if (cookie_index != -1) {
-      const cookie_score = document.cookie
-        .split(';')
-        [cookie_index].trim()
-        .slice(6);
-      if (cookie_score.length === scenes.length) {
-        for (var i = 0; i < cookie_score.length; i++) {
-          scores[scenes[i]] = parseInt(cookie_score[i], 16);
-        }
-      } else {
-        console.log('Error: Saved score(s) may be outdated. Will not be used!');
-      }
+    function getScore(scene) {
+      return parseInt(localStorage.getItem(scene));
     }
 
     var scenes_index = 0;
 
-    const top_scores_text = this.add
-      .text(
-        400,
-        295,
-        'Town: ' +
-          scores.town +
-          '/10\n' +
-          'Restaurant: ' +
-          scores.restaurant +
-          '/10\n' +
-          'Home: ' +
-          scores.home +
-          '/10\n' +
-          'Family: ' +
-          scores.family +
-          '/10\n' +
-          'Greetings: ' +
-          scores.greetings +
-          '/10\n' +
-          'All: ' +
-          scores.all +
-          '/10',
-        {
-          font: '50px Mukta',
-          color: '#479D76',
-          align: 'right',
-        }
-      )
+    // const top_scores_text = this.add
+    //   .text(
+    //     400,
+    //     295,
+    //     'Town: ' +
+    //       scores.town +
+    //       '/10\n' +
+    //       'Restaurant: ' +
+    //       scores.restaurant +
+    //       '/10\n' +
+    //       'Home: ' +
+    //       scores.home +
+    //       '/10\n' +
+    //       'Family: ' +
+    //       scores.family +
+    //       '/10\n' +
+    //       'Greetings: ' +
+    //       scores.greetings +
+    //       '/10\n' +
+    //       'All: ' +
+    //       scores.all +
+    //       '/10',
+    //     {
+    //       font: '50px Mukta',
+    //       color: '#479D76',
+    //       align: 'right',
+    //     }
+    //   )
+    //   .setOrigin(0.5);
+
+    // right-aligned for now as text is hard to read left-aligned
+    const scenesText = this.add
+      .text(340, 295, 'Home:\n'
+                    + 'Family:\n'
+                    + 'Greetings:\n'
+                    + 'Town:\n'
+                    + 'Restaurant:\n'
+                    + 'All:', 
+      {
+        font: '50px Mukta',
+        color: '#479D76',
+        align: 'right',
+        fixedWidth: '260',
+      })
       .setOrigin(0.5);
 
-    if (scenes.length > 6) {
-      // Up arrow (placeholders)
-      this.add
-        .text(590, 130, '/\\', {
-          font: '30px Trebuchet MS',
-          color: '#000000',
-        })
-        .setOrigin(0.5)
-        .setInteractive()
-        .on('pointerdown', () => {
-          scenes_index = scrollUpScores(scenes_index, top_scores_text);
-        });
+    const scoresText = this.add
+      .text(540, 295, getScore('home') + '/10\n'
+                    + getScore('family') + '/10\n'
+                    + getScore('greetings') + '/10\n'
+                    + getScore('town') + '/10\n'
+                    + getScore('restaurant') + '/10\n'
+                    + getScore('all') + '/10',
+      {
+        font: '50px Mukta',
+        color: '#479D76',
+        align: 'right',
+      })
+      .setOrigin(0.5);
 
-      // Down arrow
-      this.add
-        .text(590, 460, '\\/', {
-          font: '30px Trebuchet MS',
-          color: '#000000',
-        })
-        .setOrigin(0.5)
-        .setInteractive()
-        .on('pointerdown', () => {
-          scenes_index = scrollDownScores(scenes_index, top_scores_text);
-        });
-    }
+    // Will be updated once more scenes are added
+    // if (scenes.length > 6) {
+    //   // Up arrow (placeholders)
+    //   this.add
+    //     .text(590, 130, '/\\', {
+    //       font: '30px Trebuchet MS',
+    //       color: '#000000',
+    //     })
+    //     .setOrigin(0.5)
+    //     .setInteractive()
+    //     .on('pointerdown', () => {
+    //       scenes_index = scrollUpScores(scenes_index, top_scores_text);
+    //     });
+
+    //   // Down arrow
+    //   this.add
+    //     .text(590, 460, '\\/', {
+    //       font: '30px Trebuchet MS',
+    //       color: '#000000',
+    //     })
+    //     .setOrigin(0.5)
+    //     .setInteractive()
+    //     .on('pointerdown', () => {
+    //       scenes_index = scrollDownScores(scenes_index, top_scores_text);
+    //     });
+    // }
 
     const reset_scores = this.add
       .text(400, 540, 'Reset Scores', {
@@ -164,11 +166,10 @@ export default class ScoresScene extends Phaser.Scene {
       .setInteractive()
       .on('pointerup', () => {
         reset_scores.setColor('#000000');
-        if (reset_scores.text == 'Are you sure?') {
-          for (var scene in scores) {
-            scores[scene] = 0;
+        if (reset_scores.text === 'Are you sure?') {
+          for (const scene of scenes) {
+            localStorage.setItem(scene, 0);
           }
-          updateScoreCookie();
           this.scene.start('scores');
         } else {
           reset_scores.setText('Are you sure?');
