38ee8f70fe1dadc0619a9c50b58e57abdca90551
dannyyua <dannyyua@gmail.com>
2021-06-17 19:27:21 -0700

    added feedback link on exit page

--- src/scenes/ExitScene.js
+++ src/scenes/ExitScene.js
@@ -13,5 +13,34 @@ export default class ExitScene extends Phaser.Scene {
 
   create() {
     this.background = this.add.image(400, 300, 'exitBackground');
+
+    const feedbackPrompt = this.add
+      .text(230, 500, 'Got feedback?', {
+        font: '36px Mukta',
+        color: '#593B2B',
+      })
+      .setOrigin(0.5);
+
+    const feedbackLink = this.add
+      .text(420, 500, 'Click here!', {
+        font: '36px Mukta',
+        color: '#3366BB',
+      })
+      .setInteractive({ useHandCursor: true })
+      .setOrigin(0.5)
+      .on('pointerup', () => {
+        window.open('https://www.surveymonkey.ca/r/N6XBHLY');
+      })
+      .on('pointerover', () => {
+        linkUnderline.setVisible(true);
+      })
+      .on('pointerout', () => {
+        linkUnderline.setVisible(false);
+      });
+
+    const linkUnderline = this.add
+      .line(420, 514, 0, 0, 154, 0, '0x3366BB');
+
+    linkUnderline.setVisible(false);
   }
 }
